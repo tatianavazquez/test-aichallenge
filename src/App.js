dfb14cd665cdb3d9f537a74d1e4f3d3fcd4adfc7
tatianavazquezvl <tatiana@virtuallatinos.com>
2025-11-24 18:25:28 -0300

    review

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AVISAAAA ??
+          review?
         </p>
         <a
           className="App-link"
