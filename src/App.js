af1915c0ce1f30fa52fc3a4fa573199a7275d036
tatianavazquezvl <tatiana@virtuallatinos.com>
2025-11-24 18:27:46 -0300

    change

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-log" alt="logo" />
         <p>
           review?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>
+        This is an example.
+       </div>
       </header>
     </div>
   );
