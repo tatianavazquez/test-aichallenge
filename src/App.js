8fdf86f4803c547b9807fcdee63982ad0595dcf2
tatianavazquezvl <tatiana@virtuallatinos.com>
2025-11-24 18:38:38 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-log" alt="logo" />
+        <img src={logo} className="App-logoooo" alt="logo" />
         <p>
           review?
         </p>
