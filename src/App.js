45d23d4cd262499b7d437c9e59e94af6bcda3d1a
tatianavazquezvl <tatiana@virtuallatinos.com>
2025-11-25 10:33:06 -0300

    test

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           AVISAAAA ??
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
       </header>
     </div>
   );
