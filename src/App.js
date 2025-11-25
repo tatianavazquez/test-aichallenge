aaab3b35fe0f3ac13ad94b479bd2d90d6a7a5de7
tatianavazquezvl <tatiana@virtuallatinos.com>
2025-11-25 16:57:02 -0300

    flex

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           AVISAAAA ??
         </p>
-     <p style={{ color: "red", fontWeight: "bold", fontSize: "10px", flex: 1 }}>trying</p>
+     <p style={{ color: "red", fontWeight: "bold", fontSize: "10px" }}>trying</p>
       </header>
     </div>
   );
